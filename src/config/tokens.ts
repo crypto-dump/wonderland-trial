7c5dc7a08942507ebd16e01bcb54c7d7a1ce0316
crypto-dump <jt1990830@gmail.com>
2024-06-24 22:06:27 -0400

    fix: update amoy token contract addresses

--- src/config/tokens.ts
+++ src/config/tokens.ts
@@ -10,10 +10,10 @@ const DAI_SEPOLIA: ERC20Token = {
 };
 
 const DAI_AMOY: ERC20Token = {
-  name: 'DAI',
+  name: 'LINK',
   decimals: 18,
-  symbol: 'DAI',
-  address: '0x1D70D57ccD2798323232B2dD027B3aBcA5C00091',
+  symbol: 'LINK',
+  address: '0x0fd9e8d3af1aaee056eb9e802c3a762a667b1904',
   chainId: polygonAmoy.id,
 };
 
@@ -29,7 +29,7 @@ const USDC_AMOY: ERC20Token = {
   name: 'USDC',
   decimals: 6,
   symbol: 'USDC',
-  address: '0xC891481A0AaC630F4D89744ccD2C7D2C4215FD47',
+  address: '0x41e94eb019c0762f9bfcf9fb1e58725bfb0e7582',
   chainId: polygonAmoy.id,
 };
 
